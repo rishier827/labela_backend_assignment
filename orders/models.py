e87b2af74139048e7ebd54c95dfaa4beb79d9f1c
rishier827 <rsnramasinghe@gmail.com>
2024-01-20 11:46:12 +0530

    models finalized

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    cartItems = models.ManyToManyField('products.Product')
+    deliver_at = models.DateTimeField(null=True)
+    is_delivered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}\'s Orders'
