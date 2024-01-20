af823606e685d830d00add79960e192d3c01b623
rishier827 <rsnramasinghe@gmail.com>
2024-01-20 11:37:27 +0530

    added carts

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    products = models.ManyToManyField('products.Product', through='CartProduct')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}\'s Cart'
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.quantity} of {self.product.name} in cart {self.cart.id}'
