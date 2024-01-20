8ebcb97fb30cf3a0a196295a0f767fd899d12bb6
rishier827 <rsnramasinghe@gmail.com>
2024-01-20 11:23:56 +0530

    user, product models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False)
+    price = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- users/models.py
+++ users/models.py
@@ -1,8 +1,15 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     role = models.CharField(max_length=50, null=True, blank=True)
+    groups = models.ManyToManyField(Group, blank=True, related_name="customuser_groups")
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name="customuser_user_permissions")
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
 
     def __str__(self):
         return self.username
