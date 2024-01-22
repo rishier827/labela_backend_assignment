242d7030e8b471ed4469cf0c2701366816175260
rishier827 <rsnramasinghe@gmail.com>
2024-01-22 19:48:33 +0530

    added pagination for the product list

--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 from rest_framework import permissions, status, authentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product
 from .serializers import ProductSerializer
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10 
 
 class ProductListApiView(APIView):
 
@@ -36,9 +40,11 @@ class ProductListApiView(APIView):
         '''
         List all products
         '''
+        paginator = CustomPagination()
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        context = paginator.paginate_queryset(products, request)
+        serializer = ProductSerializer(context, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
 class ProductDetailsApiView(APIView):
 
